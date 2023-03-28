fbad45cfe02520b652baf8132a95a8154f47f68b
jessej-samuel <1234jessej@gmail.com>
2023-03-28 21:18:18 +0530

    ⚒️ fix: make tw export all classes

--- components/ContentBox.jsx
+++ components/ContentBox.jsx
@@ -1,18 +1,55 @@
-const ContentBox = ({ title, content, color = "neutral" }) => {
-  return (
-    <div
-      className={`min-h-[520px] mx-24 bg-gradient-to-t from-${color}/75 via-${color}/10 to-white px-8 rounded-3xl`}
-    >
-      {/* Backdrop text below */}
-      <span
-        className={`font-serif font-medium text-${color}/20 text-8xl absolute -translate-y-1/2`}
+const ContentBox = ({ title, content, color = "sectiveyellow" }) => {
+  if (color === "selectiveyellow")
+    return (
+      <div
+        className={`min-h-[520px] mx-24 bg-gradient-to-t from-selectiveyellow/75 via-selectiveyellow/10 to-white px-8 rounded-3xl`}
       >
-        {title}
-      </span>
-      <h1 className={`text-5xl font-semibold text-${color} mb-12`}>{title}</h1>
-      <p className={`text-3xl leading-10`}>{content}</p>
-    </div>
-  );
+        {/* Backdrop text below */}
+        <span
+          className={`font-serif font-medium text-selectiveyellow/20 text-8xl absolute -translate-y-1/2`}
+        >
+          {title}
+        </span>
+        <h1 className={`text-5xl font-semibold text-selectiveyellow mb-12`}>
+          {title}
+        </h1>
+        <p className={`text-3xl leading-10`}>{content}</p>
+      </div>
+    );
+  else if (color === "royalblue")
+    return (
+      <div
+        className={`min-h-[520px] mx-24 bg-gradient-to-t from-royalblue/75 via-royalblue/10 to-white px-8 rounded-3xl`}
+      >
+        {/* Backdrop text below */}
+        <span
+          className={`font-serif font-medium text-royalblue/20 text-8xl absolute -translate-y-1/2`}
+        >
+          {title}
+        </span>
+        <h1 className={`text-5xl font-semibold text-royalblue mb-12`}>
+          {title}
+        </h1>
+        <p className={`text-3xl leading-10`}>{content}</p>
+      </div>
+    );
+  else if (color === "brightred")
+    return (
+      <div
+        className={`min-h-[520px] mx-24 bg-gradient-to-t from-brightred/75 via-brightred/10 to-white px-8 rounded-3xl`}
+      >
+        {/* Backdrop text below */}
+        <span
+          className={`font-serif font-medium text-brightred/20 text-8xl absolute -translate-y-1/2`}
+        >
+          {title}
+        </span>
+        <h1 className={`text-5xl font-semibold text-brightred mb-12`}>
+          {title}
+        </h1>
+        <p className={`text-3xl leading-10`}>{content}</p>
+      </div>
+    );
 };
 
 export default ContentBox;
