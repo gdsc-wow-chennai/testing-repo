cc9653018fdb6c197d328196e5a7eca314d40085
jessej-samuel <1234jessej@gmail.com>
2023-03-28 20:04:29 +0530

    add: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        royalblue: "#4285F4",
+        brightred: "#DB4437",
+      },
+    },
     fontFamily: {
       sans: ["Poppins", "sans-serif"],
     },
