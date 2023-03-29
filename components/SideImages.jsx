65258bf5d98869cfff21ca3c1eed0dc4a898bbb1
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-03-29 19:50:28 +0530

    Made the landing page responsive

--- components/SideImages.jsx
+++ components/SideImages.jsx
@@ -8,12 +8,12 @@ function SideImages() {
   return (
     <div className="absolute top-0 left-0 w-full h-screen -z-10">
       <div className="flex items-start justify-between">
-        <Image src={topLeft} alt="" className="w-[35%]" />
-        <Image src={topRight} alt="" className="w-[20%]" />
+        <Image src={topLeft} alt="" className="w-[50%] md:w-[30%]" />
+        <Image src={topRight} alt="" className="w-[40%] md:w-[20%]" />
       </div>
       <div className="flex items-start justify-between ">
         <div className="absolute bottom-0 right-0 ">
-          <Image src={bottomRight} alt="" className="w-96" />
+          <Image src={bottomRight} alt="" className="w-60 md:w-96" />
         </div>
         {/* <div className="absolute -bottom-10 -left-14">
           <Image src={bottomLeft} alt="" className="w-32 opacity-30" />
