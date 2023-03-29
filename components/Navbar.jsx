55751201ca1d1a2eadd5a349df86e73bb2446894
jessej-samuel <1234jessej@gmail.com>
2023-03-29 14:38:24 +0530

    🛠️ fix: move nav more right, ux improvement

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -29,7 +29,7 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div className="invisible md:visible md:text-xl lg:text-2xl lg:visible z-50 sticky top-1 ml-72 -mr-12 border border-royalblue p-0.5 rounded-2xl shadow-md">
+    <div className="invisible md:visible md:text-xl lg:text-2xl lg:visible z-50 sticky top-5 ml-80 -mr-12 border border-royalblue p-0.5 rounded-2xl shadow-md">
       <div className=" px-6 py-4 flex flex-row justify-around bg-white rounded-2xl">
         {links.map((link, index) => {
           return <NavLink key={index} {...link} />;
@@ -45,7 +45,7 @@ const NavLink = ({ href, text }) => {
   return (
     <Link
       href={href}
-      className="font-normal hover:underline hover:underline-offset-1 hover:text-blue-500"
+      className="font-normal hover:text-blue-500 transition-colors"
     >
       {text}
     </Link>
