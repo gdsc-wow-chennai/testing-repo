ce17e63c5fa6fec6c7793ea589734f8f00ac5b62
jessej-samuel <1234jessej@gmail.com>
2023-03-29 18:24:28 +0530

    🧐 fix: make sections more filling

--- components/ContentBox.jsx
+++ components/ContentBox.jsx
@@ -9,7 +9,7 @@ const ContentBox = ({
   if (color === "selectiveyellow")
     return (
       <div
-        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-selectiveyellow/40 to-selectiveyellow/0 px-8 rounded-3xl  border-selectiveyellow`}
+        className={`min-h-[80vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-selectiveyellow/40 to-selectiveyellow/0 px-8 rounded-3xl  border-selectiveyellow`}
       >
         {/* Backdrop text below */}
         <span
@@ -37,7 +37,7 @@ const ContentBox = ({
   else if (color === "royalblue")
     return (
       <div
-        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-royalblue/40 to-royalblue/0 px-8 rounded-3xl  border-royalblue`}
+        className={`min-h-[80vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-royalblue/40 to-royalblue/0 px-8 rounded-3xl  border-royalblue`}
       >
         {/* Backdrop text below */}
         <span
@@ -65,7 +65,7 @@ const ContentBox = ({
   else if (color === "brightred")
     return (
       <div
-        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-brightred/40 to-brightred/0 px-8 rounded-3xl  border-brightred`}
+        className={`min-h-[80vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-brightred/40 to-brightred/0 px-8 rounded-3xl  border-brightred`}
       >
         {/* Backdrop text below */}
         <span
@@ -93,7 +93,7 @@ const ContentBox = ({
   else if (color === "darkgreen")
     return (
       <div
-        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-darkgreen/40 to-darkgreen/0 px-8 rounded-3xl  border-darkgreen`}
+        className={`min-h-[80vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-darkgreen/40 to-darkgreen/0 px-8 rounded-3xl  border-darkgreen`}
       >
         {/* Backdrop text below */}
         <span
