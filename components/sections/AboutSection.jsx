3c6f2e4a2adafc9643921069163093a4d5233a9e
jessej-samuel <1234jessej@gmail.com>
2023-03-29 15:21:54 +0530

    ⚒️ add: chennai skyline

--- components/sections/AboutSection.jsx
+++ components/sections/AboutSection.jsx
@@ -1,15 +1,18 @@
+import Image from "next/image";
 import ContentBox from "../ContentBox";
+import ChennaiSkyline from "assets/chennaiSkyline.svg";
 
 const AboutSection = () => {
   return (
     <div className="min-h-screen pt-24" id="about">
-      <ContentBox
-        title="About us"
-        content={
-          "GDSC Wonder of Wonders is an astronomical, country-wide, in-person, annual, student-centric technical fest maneuvered by 400+ GDSCs and 200+ colleges with over 20K events each year all over India."
-        }
-        color="selectiveyellow"
-      />
+      <ContentBox title="About us" color="selectiveyellow">
+        <p className="text-black/75 text-xl md:text-2xl lg:text-3xl leading-10  min-h-[360px] flex items-center">
+          GDSC Wonder of Wonders is an astronomical, country-wide, in-person,
+          annual, student-centric technical fest maneuvered by 400+ GDSCs and
+          200+ colleges with over 20K events each year all over India.
+        </p>
+        <Image src={ChennaiSkyline} alt="" className="relative top-1" />
+      </ContentBox>
     </div>
   );
 };
