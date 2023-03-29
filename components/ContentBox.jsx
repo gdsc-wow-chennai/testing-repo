5869f1ce71d8321b0371695139b406b09e228040
jessej-samuel <1234jessej@gmail.com>
2023-03-29 14:36:54 +0530

    🔥 ui: align site more to actual design

--- components/ContentBox.jsx
+++ components/ContentBox.jsx
@@ -1,70 +1,98 @@
 const ContentBox = ({ title, content, color = "sectiveyellow" }) => {
+  // SelectiveYellow ContentBox
   if (color === "selectiveyellow")
     return (
       <div
-        className={`min-h-[520px] mx-24 bg-gradient-to-t from-selectiveyellow/75 via-selectiveyellow/10 to-white px-8 rounded-3xl border border-opacity-25 border-selectiveyellow`}
+        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-selectiveyellow/40 to-selectiveyellow/0 px-8 rounded-3xl  border-selectiveyellow`}
       >
         {/* Backdrop text below */}
         <span
-          className={`font-serif font-medium text-selectiveyellow/20 text-8xl absolute -translate-y-1/2`}
+          className={`font-serif font-bold text-opacity-0 text-selectiveyellow/10 text-5xl sm:text-6xl md:text-8xl absolute -translate-y-1/2 uppercase -translate-x-5`}
         >
           {title}
         </span>
-        <h1 className={`text-5xl font-semibold text-selectiveyellow mb-12`}>
+        <h1
+          className={`text-3xl sm:text-4xl md:text-5xl font-semibold -translate-y-1/2 text-selectiveyellow mb-12`}
+        >
           {title}
         </h1>
-        <p className={`text-3xl leading-10`}>{content}</p>
+        <p
+          className={`text-black/75 text-xl md:text-2xl lg:text-3xl leading-10  min-h-[360px] flex items-center`}
+        >
+          {content}
+        </p>
       </div>
     );
+  // RoyalBlue ContentBox
   else if (color === "royalblue")
     return (
       <div
-        className={`min-h-[520px] mx-24 bg-gradient-to-t from-royalblue/75 via-royalblue/10 to-white px-8 rounded-3xl border border-opacity-25 border-royalblue`}
+        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-royalblue/40 to-royalblue/0 px-8 rounded-3xl  border-royalblue`}
       >
         {/* Backdrop text below */}
         <span
-          className={`font-serif font-medium text-royalblue/20 text-8xl absolute -translate-y-1/2`}
+          className={`font-serif font-bold text-opacity-0 text-royalblue/10 text-5xl sm:text-6xl md:text-8xl absolute -translate-y-1/2 uppercase -translate-x-5`}
         >
           {title}
         </span>
-        <h1 className={`text-5xl font-semibold text-royalblue mb-12`}>
+        <h1
+          className={`text-3xl sm:text-4xl md:text-5xl font-semibold -translate-y-1/2 text-royalblue mb-12`}
+        >
           {title}
         </h1>
-        <p className={`text-3xl leading-10`}>{content}</p>
+        <p
+          className={`text-black/75 text-xl md:text-2xl lg:text-3xl leading-10  min-h-[360px] flex items-center`}
+        >
+          {content}
+        </p>
       </div>
     );
+  // BrightRed ContentBox
   else if (color === "brightred")
     return (
       <div
-        className={`min-h-[520px] mx-24 bg-gradient-to-t from-brightred/75 via-brightred/10 to-white px-8 rounded-3xl border border-opacity-25 border-brightred`}
+        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-brightred/40 to-brightred/0 px-8 rounded-3xl  border-brightred`}
       >
         {/* Backdrop text below */}
         <span
-          className={`font-serif font-medium text-brightred/20 text-8xl absolute -translate-y-1/2`}
+          className={`font-serif font-bold text-opacity-0 text-brightred/10 text-5xl sm:text-6xl md:text-8xl absolute -translate-y-1/2 uppercase -translate-x-5`}
         >
           {title}
         </span>
-        <h1 className={`text-5xl font-semibold text-brightred mb-12`}>
+        <h1
+          className={`text-3xl sm:text-4xl md:text-5xl font-semibold -translate-y-1/2 text-brightred mb-12`}
+        >
           {title}
         </h1>
-        <p className={`text-3xl leading-10`}>{content}</p>
+        <p
+          className={`text-black/75 text-xl md:text-2xl lg:text-3xl leading-10  min-h-[360px] flex items-center`}
+        >
+          {content}
+        </p>
       </div>
     );
+  // DarkGreen ContentBox
   else if (color === "darkgreen")
     return (
       <div
-        className={`min-h-[520px] mx-24 bg-gradient-to-t from-darkgreen/75 via-darkgreen/10 to-white px-8 rounded-3xl border border-opacity-25 border-darkgreen`}
+        className={`min-h-[70vh] mx-12 md:mx-16 lg:mx-24 bg-gradient-to-t from-darkgreen/40 to-darkgreen/0 px-8 rounded-3xl  border-darkgreen`}
       >
         {/* Backdrop text below */}
         <span
-          className={`font-serif font-medium text-darkgreen/20 text-8xl absolute -translate-y-1/2`}
+          className={`font-serif font-bold text-opacity-0 text-darkgreen/10 text-5xl sm:text-6xl md:text-8xl absolute -translate-y-1/2 uppercase -translate-x-5`}
         >
           {title}
         </span>
-        <h1 className={`text-5xl font-semibold text-darkgreen mb-12`}>
+        <h1
+          className={`text-3xl sm:text-4xl md:text-5xl font-semibold -translate-y-1/2 text-darkgreen mb-12`}
+        >
           {title}
         </h1>
-        <p className={`text-3xl leading-10`}>{content}</p>
+        <p
+          className={`text-black/75 text-xl md:text-2xl lg:text-3xl leading-10  min-h-[360px] flex items-center`}
+        >
+          {content}
+        </p>
       </div>
     );
 };
