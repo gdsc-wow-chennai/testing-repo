56a77c0148da806ec7a7b14b8e15d68a58227abc
AbhijithGanesh <git.watch.Abhijith@outlook.com>
2023-04-02 22:22:48 +0530

    Addition of new SEO
    Signed-off-by: AbhijithGanesh <git.watch.Abhijith@outlook.com>

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -2,13 +2,14 @@ import Head from "next/head";
 
 let SeoComponent = ({ title, description, image }) => {
     return (<Head>
-        <meta property="title" content={title} />
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
         <meta property="og:title" content={title} />
-        <meta property="description" content={description} />
         <meta property="image" content={image} />
         <meta property="og:image" content={image} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="keywords" content="Student Networking, GoogleStudents, CampusConnect"/>
+        <meta name="keywords" content="Student Networking, GoogleStudents, CampusConnect" />
+        <meta name="robots" content="index, follow" />
     </Head>)
 }
 
