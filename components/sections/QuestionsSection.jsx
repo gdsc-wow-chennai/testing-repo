4630b92501facaf2e4e90fa9e028f1e76839e718
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-04-03 10:48:11 +0530

    Added Icon for Questions

--- components/sections/QuestionsSection.jsx
+++ components/sections/QuestionsSection.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const QuestionsSection = () => {
   return (
     <div className="min-h-screen pt-14 md:pt-24 " id="questions">
-      <div className="absolute -translate-y-32  md:translate-y-[100rem] md:-bottom-[53rem] -left-14">
+      <div className="absolute -translate-y-32  md:translate-y-[174rem] md:-bottom-[53rem] -left-14">
         <Image src={bottomLeft} alt="" className="w-28 md:w-32 opacity-30" />
       </div>
       <ContentBox
