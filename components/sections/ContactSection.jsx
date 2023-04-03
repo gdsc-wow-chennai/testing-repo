393937281bd196c62e40f3605f10aba512d139fb
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-04-03 10:49:12 +0530

    Added icon for Contact Us

--- components/sections/ContactSection.jsx
+++ components/sections/ContactSection.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 const ContactSection = () => {
   return (
     <div className="min-h-screen pt-14 md:pt-24" id="contact">
-      <div className="absolute -translate-y-32 md:translate-y-[10rem] md:-bottom-[53rem] -left-14">
+      <div className="absolute -translate-y-32 md:translate-y-[216rem] md:-bottom-[53rem] -left-14">
         <Image src={bottomLeft} alt="" className="w-28 md:w-32 opacity-30" />
       </div>
       <ContentBox
