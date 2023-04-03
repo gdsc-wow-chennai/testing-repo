b37d37d9a2e5a8283c246538e7e94301c00cc397
Mohammad Shaad Shaikh <mohammadshaadshaikh@gmail.com>
2023-04-04 01:40:55 +0530

    Modified the preoader

--- components/Preloader.jsx
+++ components/Preloader.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import PreloaderGif from "../assets/preloader.gif";
+import PreloaderGif from "../assets/preloader-unscreen.gif";
 
 const Preloader = () => {
   return (
     <div className="flex items-center justify-center w-screen h-full bg-white fixed top-0 left-0 z-9999">
-      <Image src={PreloaderGif} alt="Loading..." />
+      <Image src={PreloaderGif} alt="Loading..." className="w-1/2"/>
     </div>
   );
 };
